9a9d34b9ac6e6cf0f5eccc18df2d9b28e60206b8
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-07-22 23:47:37 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int f(int ind, int prevInd, int n,vector<vector<int>>& dp,vector<int>& nums){
+        if(ind==n) return 0;
+        if(dp[prevInd][ind]!=-1) return dp[prevInd][ind];
+        int len = f(ind+1,prevInd,n,dp,nums);
+        if(prevInd==0 || nums[prevInd-1]<nums[ind]){
+            len = max(len,1+f(ind+1,ind+1,n,dp,nums));
+        }
+        return dp[prevInd][ind]=len;
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(0,0,n,dp,nums);
+        
+    }
+};
